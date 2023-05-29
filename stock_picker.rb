edb3b7fcc9214d2f9008edad79adc4c59e0bf664
Jelson Alfarero <heyjelson@gmail.com>
2023-05-30 06:34:13 +0800

    refactor the stock picker code

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,14 @@
 arr = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(arr)
-    stock_prices = [], best_price_tobuy = [], best_price_tosell =
-    [], best_prices = []
+    stock_prices = [], best_price_tobuy = 0, best_price_tosell =
+    0, best_prices = 0
     stock_prices = arr.filter {|e| e != arr.min && e != arr.max}.
     rotate(-1)
     best_price_tobuy = stock_prices.index(stock_prices.min).to_s.
     chars.map {|e| e.to_i}
     best_price_tosell = stock_prices.index(stock_prices.max).to_s
     .chars.map {|e| e.to_i}
-    profit = best_price_tosell - best_price_tobuy
     best_prices = best_price_tobuy.concat(best_price_tosell)
     best_prices 
 end
